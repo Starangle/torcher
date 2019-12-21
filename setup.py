111f734480dc1b85225ef5d915c8b4df828a844c
binsu <binsu.cs@qq.com>
2019-12-21 13:54:50 +0800

    0.0.4

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="torcher",
-    version="0.0.3",
+    version="0.0.4",
     author="binsu",
     author_email="binsu.cs@qq.com",
     description="A pytorch model training util",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/Starangle/torcher",
     packages=setuptools.find_packages(),
     classifiers=[
